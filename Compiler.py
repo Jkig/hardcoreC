375a31ebad02cd03d69259e87995979a328b5ee1
Derek <DerekLeroux47@gmail.com>
2026-03-31 20:52:14 -0700

    working on the higher level parts of the compiler

---
+++ Compiler.py
@@ -0,0 +1,244 @@
+import os
+import sys
+
+def tokenize_file(input_filename, output_filename):
+    try:
+        input_file = open(input_filename, "r")
+    except IOError:
+        print("Error opening input file")
+        return
+
+    try:
+        output_file = open(output_filename, "w")
+    except IOError:
+        print("Error opening output file")
+        input_file.close()
+        return
+
+    def peek():
+        pos = input_file.tell()
+        c = input_file.read(1)
+        input_file.seek(pos)
+        return c
+
+    def peek2():
+        pos = input_file.tell()
+        c = input_file.read(2)
+        input_file.seek(pos)
+        return c
+
+    def write_token(tok):
+        output_file.write(tok + " ")
+
+    while True:
+        c = input_file.read(1)
+        if c == "":
+            break  # EOF
+
+        # --- skip whitespace ---
+        if c.isspace():
+            continue
+
+        # --- skip comments ---
+        if c == '/':
+            next_char = peek()
+
+            if next_char == '/':  # single-line
+                input_file.read(1)  # consume second '/'
+                while True:
+                    c = input_file.read(1)
+                    if c == "" or c == '\n':
+                        break
+                continue
+
+            elif next_char == '*':  # multi-line
+                input_file.read(1)  # consume '*'
+                while True:
+                    c = input_file.read(1)
+                    if c == "":
+                        break
+                    if c == '*' and peek() == '/':
+                        input_file.read(1)  # consume '/'
+                        break
+                continue
+
+            else:
+                write_token('/')
+                continue
+
+        # --- identifier / keyword ---
+        if c.isalpha() or c == '_':
+            token = c
+            while True:
+                nxt = peek()
+                if nxt.isalnum() or nxt == '_':
+                    token += input_file.read(1)
+                else:
+                    break
+            write_token(token)
+            continue
+
+        # --- number (simple integers for now) ---
+        if c.isdigit():
+            token = c
+            while True:
+                nxt = peek()
+                if nxt.isdigit():
+                    token += input_file.read(1)
+                else:
+                    break
+            write_token(token)
+            continue
+
+        # --- multi-char operators ---
+        two = c + peek()
+        if two in {"==", "!=", "<=", ">=", "&&", "||", "++", "--", "->"}:
+            input_file.read(1)  # consume second char
+            write_token(two)
+            continue
+
+        # --- single-char operators / punctuation ---
+        if c in "{}()[]:;,.+-*/%=<>!?&|^~":
+            write_token(c)
+            continue
+
+        # --- skip preprocessor directives ---
+        if c == '#':
+            while True:
+                c = input_file.read(1)
+                if c == "" or c == '\n':
+                    break
+            continue
+
+        # --- char literal ---
+        if c == "'":
+            token = c
+
+            while True:
+                nxt = input_file.read(1)
+                if nxt == "":
+                    raise ValueError("Unterminated char literal")
+
+                token += nxt
+
+                if nxt == '\\':
+                    escaped = input_file.read(1)
+                    if escaped == "":
+                        raise ValueError("Unterminated char literal")
+                    token += escaped
+                    continue
+
+                if nxt == "'":
+                    break
+
+            write_token(token)
+            continue
+
+        # --- string literal ---
+        if c == '"':
+            token = c
+
+            while True:
+                nxt = input_file.read(1)
+                if nxt == "":
+                    raise ValueError("Unterminated string literal")
+
+                token += nxt
+
+                if nxt == '\\':
+                    escaped = input_file.read(1)
+                    if escaped == "":
+                        raise ValueError("Unterminated string literal")
+                    token += escaped
+                    continue
+
+                if nxt == '"':
+                    break
+
+            write_token(token)
+            continue
+
+        raise ValueError(f"Unexpected character: {c}")
+
+    input_file.close()
+    output_file.close()
+
+
+def to_dasm(file_name: str):
+    # This is going to be fun!
+    # This is wehre I make sure I have a sufficient ISA
+
+    dasm_file_name = ".".join(file_name.split(".")[:-1] + ["dasm"])
+    tokenized_file_name = ".".join(file_name.split(".")[:-1] + ["tokenized"])
+    
+    try:
+        input_file = open(tokenized_file_name, "r")
+    except IOError:
+        print("Error opening input file")
+        return
+
+    try:
+        output_file = open(dasm_file_name, "w")
+    except IOError:
+        print("Error opening output file")
+        input_file.close()
+        return
+    
+    
+    input_file.close()
+    output_file.close()
+    
+
+
+
+
+def compile(file_name: str, output_file_name: str, skip_cleanup: bool):
+    tokenized_file_name = ".".join(file_name.split(".")[:-1] + ["tokenized"])
+    tokenize_file(file_name, tokenized_file_name) # is this the right term?
+
+    to_dasm(file_name)
+
+    # undeceided on if this should be done in c or maybe just here in python
+    # assemble_binary(file_name, output_file_name)
+
+    if not skip_cleanup:
+        os.remove(tokenized_file_name)
+
+
+if __name__ == "__main__":
+    input_file = None
+    output_file = None
+    save_temps = False
+    args = sys.argv[1:]
+
+    i = 0# skip python3 Compiler.py
+    while i < len(args):
+        arg = args[i]
+        print(arg)
+        if arg == "-o":
+            i += 1
+            if i < len(args):
+                output_file = args[i]
+            else:
+                print("Usage: python3 Compiler.py <file_name>.c -o <binary_file_name> [options]")
+                print("   -save-temps: don't delete the temporary files")
+                sys.exit(1)
+        elif arg == "-save-temps":
+            save_temps = True
+        else:
+            # assume it's the input file
+            if input_file is None:
+                input_file = arg
+            else:
+                print(f"Unknown argument: {arg}")
+                sys.exit(1)
+        i += 1
+    
+    if input_file is None or output_file is None:
+        print(sys.argv)
+        print(f"File names not set")
+        print(f"   input_file: {input_file}\n   output_file: {output_file}")
+        sys.exit(1)
+
+    compile(input_file, output_file, save_temps)
+
